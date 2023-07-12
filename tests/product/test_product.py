5cdbace230160de1032cba6514b4c8f044facfd8
Vitor Wagner <vitormwagner@gmail.com>
2023-07-12 14:47:58 -0300

    Feat: :sparkles: Added tests for `Product` object constructor

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1",
+        "HMS Erebus",
+        "Pembroke Dock",
+        "07/06/1826",
+        "22/04/1848",
+        "1",
+        "Keep in a cool and dry place",
+    )
+    assert product.id == "1"
+    assert product.product_name == "HMS Erebus"
+    assert product.company_name == "Pembroke Dock"
+    assert product.manufacturing_date == "07/06/1826"
+    assert product.expiration_date == "22/04/1848"
+    assert product.serial_number == "1"
+    assert product.storage_instructions == "Keep in a cool and dry place"
