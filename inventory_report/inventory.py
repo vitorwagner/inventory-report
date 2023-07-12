0e2c916bf0b550ebf7e224d3f1ae48dd3eb41828
Vitor Wagner <vitormwagner@gmail.com>
2023-07-12 16:01:24 -0300

    Feat: :sparkles: Adds `Inventory` class

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self._data = data or []
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
